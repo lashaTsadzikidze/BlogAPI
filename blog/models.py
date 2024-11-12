2c2842482c02c165a231579ba6bd2dcd3e8f6611
lasha1222 <lasha.tsadzikidze@geolab.edu.ge>
2024-11-12 15:34:50 +0400

    create models and make migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
